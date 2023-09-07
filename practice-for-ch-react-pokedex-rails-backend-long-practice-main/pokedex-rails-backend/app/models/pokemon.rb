f42579f4f4e0b3627de9f2e480884a69fdefaefd
Dennis Lee <dennislee1499@gmail.com>
2023-09-07 14:26:38 -0400

    finished pokemon validations

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/pokemon.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/models/pokemon.rb
@@ -1,2 +1,44 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Pokemon < ApplicationRecord
+    validates :number, uniqueness: {message: "'%{value}' is already in use."}, length: {minimum: 1}
+    validates :name, uniqueness: {message: "'%{value}' is already in use."}, length: {in: 3..255}
+    validates :attack, length: {in: 0..100}
+    validates :defense, length: {in: 0..100}
+    validates :poke_type, inclusion: { in: TYPES, message: "'${value}' is not a valid pokemon type" }
+    validates :image_url, presence: true 
+    validates :captured, inclusion: [true, false]
+
+      TYPES = [
+    'fire',
+    'electric',
+    'normal',
+    'ghost',
+    'psychic',
+    'water',
+    'bug',
+    'dragon',
+    'grass',
+    'fighting',
+    'ice',
+    'flying',
+    'poison',
+    'ground',
+    'rock',
+    'steel'
+  ].sort.freeze
+
 end
